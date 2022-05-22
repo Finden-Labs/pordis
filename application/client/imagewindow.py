cebf70ffc9143ed97d67b2b931171cbc00a7ed91
sailboat-anon <sailboatanon@protonmail.com>
2022-05-22 14:17:17 -0700

    picklist works

--- application/client/imagewindow.py
+++ application/client/imagewindow.py
@@ -1,11 +1,38 @@
-from tkinter import *  
-from tkinter.ttk import *
+import tkinter as tk
+from tkinter import ttk
 from PIL import ImageTk, Image  
+from tkinter.messagebox import showinfo
 
-root = Tk()  
+root = tk.Tk()  
 root.title('Go Crazy!')
-canvas = Canvas(root, width = 475, height = 466)  
+frame = tk.Frame(root)
+
+
+frame.grid(row = 0, column = 0, sticky="NSEW")
+canvas = tk.Canvas(frame, width=600, height=600)  
 canvas.pack()  
 img = ImageTk.PhotoImage(Image.open("/home/sba/projects/pordis/application/client/ball.png"))  
-canvas.create_image(20, 20, anchor=NW, image=img) 
+canvas.create_image(300, 300, image=img)  # x,y should be a ratio of canvas width/height
+
+frame.grid(row = 1, column = 0,sticky = "NESW")
+snippet_list = ["Arc", "Ellipse", "Gradient", "Spiral", "Text"]
+selected_snippet = tk.StringVar()
+picklist = ttk.Combobox(frame, values = snippet_list, state = "readonly", textvariable=selected_snippet)
+picklist.set("Filter")
+
+picklist.pack(padx = 5, pady = 5)
+
+frame.grid(row = 1, column = 1, sticky = "W")
+button = tk.Button(frame, text = "Apply", command = set,
+                fg = "red",
+                bd = 2, bg = "light blue", relief = "groove")
+button.pack(pady = 5)
+
+def apply_snippet(event):
+    showinfo(
+        title='Result',
+        message=f'You selected {selected_snippet.get()}!'
+    )
+
+picklist.bind('<<ComboboxSelected>>', apply_snippet)
 root.mainloop()
